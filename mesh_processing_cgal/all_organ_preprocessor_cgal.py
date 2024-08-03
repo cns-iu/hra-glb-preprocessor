b70f65895bf97cb3298223529faa41c7dfeefa46
Lu <luchen_catherine@outlook.com>
2024-08-02 21:15:17 -0400

    Python wrapper

---
+++ mesh_processing_cgal/all_organ_preprocessor_cgal.py
@@ -0,0 +1,74 @@
+import os
+import json
+import requests
+import subprocess
+import argparse
+from glb_parser import glb_parser_all
+
+
+# Naive Hashing for url
+def convert_url_to_file(url):
+    # Replace illegal chars using underscore
+    illegal_chars = ['/', ':', '@', '&', '*']
+    
+    for c in illegal_chars:
+        url = url.replace(c, '_')
+    
+    return url
+
+# Get the latest version models
+def download_model(api_url, output_folder):
+    """A function to get download URLs for all 3D Reference Objects in the HRA
+    """
+
+    # send request
+    response = requests.get(api_url).text
+    data = json.loads(response)
+
+    # Make folder for GLB files or check if already present
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Get download URLs
+    for organ in data:
+        glb_url = organ['object']['file']
+        file_name = convert_url_to_file(glb_url)
+        file_path = os.path.join(output_folder, file_name)
+        
+        # Already downloaded, skip it. 
+        if os.path.exists(file_path):
+            continue
+        
+        if glb_url:
+            glb_response = requests.get(glb_url)
+            
+            # Download model
+            if glb_response.status_code == 200:
+                with open(file_path, "wb") as file:
+                    file.write(glb_response.content)
+                    print(f"Downloaded {file_name}")
+
+if __name__ == "__main__":
+
+    # get data from HRA API endpoint
+    endpoint = "https://apps.humanatlas.io/api/v1/reference-organs"
+
+    # Use `argparse` to build URL
+    parser = argparse.ArgumentParser(
+        description="CGAL Preprocessor for GLB files")
+    parser.add_argument("--preproceesed_models_stage_1", type=str,
+                        help="Folder to save preprocessed GLB models from Stage 1", default="../all_preprocessed_glb_models/")
+    parser.add_argument("--output_off_model_dir", type=str,
+                        help="Directory to the preprocessed OFF models", default="all_preprocessed_off_models_cgal/")
+    args, unknown = parser.parse_known_args()
+
+
+    preproceesed_models_stage_1 = args.preproceesed_models_stage_1
+    plain_model_dir = "./temp_plain_off_model_dir"
+    output_off_model_dir = args.output_off_model_dir
+    glb_parser_all(preproceesed_models_stage_1, plain_model_dir)
+
+
+
+    cmd = ['./build/mesh_hole_filling', plain_model_dir, output_off_model_dir]
+    subprocess.run(cmd)
+    
